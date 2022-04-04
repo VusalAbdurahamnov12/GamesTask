385268d6964a56ed732385cf5eb19436f1bc3e0b
test <sh13ld001@gmail.com>
2022-04-05 03:55:12 +0400

    exception

---
+++ GameClass/Exceptions/AlreadyBuyedException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameClass.Exceptions
+{
+    internal class AlreadyBuyedException
+    {
+    }
+}
---
+++ GameClass/Exceptions/NotAvabaibleException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameClass.Exceptions
+{
+    public  class NotAvabaibleException:Exception
+    {
+        public NotAvabaibleException(string message) : base(message) { }
+    }
+}
